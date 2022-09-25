680e9db80d6c93e18f2dbc054dfb91eff944d002
Patrick Eugene de Farias <30466561+patrickfarias@users.noreply.github.com>
2022-09-25 17:41:24 -0300

    start

---
+++ src/test/java/br/com/farias/patrick/restassured/RestAssuredApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.farias.patrick.restassured;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestAssuredApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
