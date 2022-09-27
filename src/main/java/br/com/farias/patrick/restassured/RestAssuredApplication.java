5fabba926f6695e2edd9204fbeaed59abfcf331d
Patrick Eugene de Farias <30466561+patrickfarias@users.noreply.github.com>
2022-09-27 17:32:02 -0300

    Add files

--- src/main/java/br/com/farias/patrick/restassured/RestAssuredApplication.java
+++ src/main/java/br/com/farias/patrick/restassured/RestAssuredApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RestAssuredApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(RestAssuredApplication.class, args);
 	}
 
---
+++ src/test/java/br/com/farias/patrick/restassured/OlaMundoTest.java
@@ -0,0 +1,96 @@
+package br.com.farias.patrick.restassured;
+
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import org.hamcrest.Matchers;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.request;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.endsWith;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.startsWith;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OlaMundoTest {
+
+    @Test
+    public void testOlaMundo() {
+        Response response = request(Method.GET, "http://restapi.wcaquino.me:80/ola");
+        Assert.assertTrue(response.getBody().asString().equals("Ola Mundo!"));
+        Assert.assertTrue(response.statusCode() == 200);
+        Assert.assertTrue("O status code deveria ser 200", response.statusCode() == 200);
+        Assert.assertEquals(200, response.statusCode());
+
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerOutrasFormasRestAssured() {
+        Response response = request(Method.GET, "http://restapi.wcaquino.me/ola");
+        ValidatableResponse validacao = response.then();
+        validacao.statusCode(200);
+
+        get("http://restapi.wcaquino.me/ola").then().statusCode(200);
+
+        given()
+                .when()
+                .get("http://restapi.wcaquino.me/ola")
+                .then()
+//			.assertThat()
+                .statusCode(200);
+    }
+
+    @Test
+    public void devoConhecerMatchersHamcrest() {
+        Assert.assertThat("Maria", Matchers.is("Maria"));
+        Assert.assertThat(128, Matchers.is(128));
+        Assert.assertThat(128, Matchers.isA(Integer.class));
+        Assert.assertThat(128d, Matchers.isA(Double.class));
+        Assert.assertThat(128d, Matchers.greaterThan(120d));
+        Assert.assertThat(128d, Matchers.lessThan(130d));
+
+        List<Integer> impares = Arrays.asList(1,3,5,7,9);
+        assertThat(impares, hasSize(5));
+        assertThat(impares, contains(1,3,5,7,9));
+        assertThat(impares, containsInAnyOrder(1,3,5,9,7));
+        assertThat(impares, hasItem(1));
+        assertThat(impares, hasItems(1, 5));
+
+        assertThat("Maria", is(not("João")));
+        assertThat("Maria", not("João"));
+        assertThat("Joaquina", anyOf(is("Maria"), is("Joaquina")));
+        assertThat("Joaquina", allOf(startsWith("Joa"), endsWith("ina"), containsString("qui")));
+    }
+
+    @Test
+    public void devoValidarBody() {
+        given()
+                .when()
+                .get("http://restapi.wcaquino.me/ola")
+                .then()
+                .statusCode(200)
+                .body(is("Ola Mundo!"))
+                .body(containsString("Mundo"))
+                .body(is(not(nullValue())));
+    }
+
+}
